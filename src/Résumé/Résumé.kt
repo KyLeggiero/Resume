cffebfdff8ffde224b9336ad3fe346406603f3c1
Ben Leggiero <BenLeggiero@Gmail.com>
2018-12-31 03:23:27 -0500

    Restructuring done; ready to start filling out content

--- src/Résumé/Résumé.kt
+++ src/Résumé/Résumé.kt
@@ -1,6 +1,7 @@
 package Résumé
 
 import Résumé.BasicRésuméJson.Content.*
+import org.bh.tools.base.collections.*
 import org.bh.tools.ui.*
 import org.bh.tools.ui.BodyText.Kind.*
 import org.bh.tools.ui.Group.Kind.*
@@ -46,6 +47,9 @@ data class Résumé(
     data class Contact(
             val name: String,
             val blogUrl: URL?
+    ): HtmlRichTextWidget<HTMLSpanElement, SingleItemCollection<HtmlElementRenderable>>(
+            htmlTagName = "span",
+            children = SingleItemCollection(blogUrl?.let { Link(href = it, text = name) } ?: PlainText(name))
     ) {
         companion object {
             operator fun invoke(from: BasicRésuméJson.Content.Contact): Contact {
